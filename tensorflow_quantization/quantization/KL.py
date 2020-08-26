Tried using KL to fix min/max for each column of weight-no improvement in MSE

tensorflow_quantization/quantization/KL.py
@@ -249,7 +249,7 @@ def get_symmetric_threshold(H_orig,NUM_QBINS,max_val,min_val,HE,HE_orig):
     res_edges=[]
     print(len(HE))
     #print("Hist :", H)
-    for i in range(NUM_QBINS/2,num_Pbins):
+    for i in range(NUM_QBINS//2,num_Pbins):
 #    if True:
  #       i = 128
         #print("Hist :", H)
@@ -288,13 +288,22 @@ def get_symmetric_threshold(H_orig,NUM_QBINS,max_val,min_val,HE,HE_orig):
         #print('Error :', entropy(ref_distribution_P,Q))
   #      print(res)
     #res[0] = 1
-    print(res)
+    #print(res)
+    if (len(res)==0):
+        if (len(H[zero_bin:]) < NUM_QBINS/2):
+            idx = len(HE) - 1            
+            return(HE_orig[zero_bin+idx], HE_orig[zero_bin-idx], idx)
+        else:
+            idx = zero_bin
+            return(HE_orig[zero_bin+idx], HE_orig[zero_bin-idx], idx)
+          
+
     min_kl=np.nanmin(res)
     idx=np.argmin(res)
     thresh_HE = res_edges[idx]
     print('Actal edge at idx :', thresh_HE)
-    print('Actual pos-edge at idx from full HE :', HE_orig[zero_bin+NUM_QBINS/2-1+idx])
-    print('Actual neg-edge at idx from full HE :', HE_orig[zero_bin-NUM_QBINS/2-idx])
+    print('Actual pos-edge at idx from full HE :', HE_orig[zero_bin+NUM_QBINS//2-1+idx])
+    print('Actual neg-edge at idx from full HE :', HE_orig[zero_bin-NUM_QBINS//2-idx])
     print("Min is {} at index {}".format(min_kl,idx))
 
     min_thresh=((idx-0.5)*min_val)/num_Pbins
@@ -302,7 +311,7 @@ def get_symmetric_threshold(H_orig,NUM_QBINS,max_val,min_val,HE,HE_orig):
     print(max_thresh)
     print(min_thresh)
     #return(max_thresh,min_thresh,idx)
-    return(HE_orig[zero_bin+NUM_QBINS/2-1+idx], HE_orig[zero_bin-NUM_QBINS/2-idx], idx)
+    return(HE_orig[zero_bin+NUM_QBINS//2-1+idx], HE_orig[zero_bin-NUM_QBINS//2-idx], idx)
     
 
 

tensorflow_quantization/quantization/constants.py
@@ -4,6 +4,9 @@ from tensorflow.python.framework import dtypes
 import tensorflow as tf
 from KL import get_pos_threshold
 from KL import get_neg_threshold
+from KL import get_symmetric_threshold
+
+from kl_divergence import KL_Divergence
 
 def q_m_qu_date(s_data, fix_max_i, Trange, sess=None, to_run=True):
     result =tf.user_ops.quant14bits_out(s_data, fix_max_i, t_range=Trange)
@@ -52,35 +55,81 @@ def generate_const(gr, all_const, _node_infor, a_min, a_max ):
 
     max_list = []
     min_list = []
+    _kl = KL_Divergence()
+
+    #### use kl-div from ilit to fix min/max of weights
+    for i in range(len(weight_float_tensor[0])):
+        hist, hist_edges = np.histogram(weight_float_tensor[:, i], bins=8001)
+        
+        th = _kl.get_threshold(hist, 
+                            hist_edges, 
+                            np.min(weight_float_tensor[:,i]),
+                            np.max(weight_float_tensor[:,i]),
+                            num_bins=8001,
+                            quantized_type=np.int8,
+                            num_quantized_bins=255)
+
+        max_list.append(th)
+        min_list.append(-th)
+        print('column, actual min/max and kl fixed min/max :', i, np.min(weight_float_tensor[:,i]), np.max(weight_float_tensor[:,i]), -th, th)
+
+    np.save('min_list', min_list)
+    np.save('max_list', max_list)
+     
+    min_list = np.load('min_list.npy')
+    max_list = np.load('max_list.npy')
+    
+    
+
+    abs_max = np.abs(max_list)
+    abs_min = np.abs(min_list)
+    ranges = np.maximum(abs_max, abs_min)
+    print('min_list :', min_list)
+    print('max_list :', max_list)
+    print('ranges :', ranges)
+
 
     '''
+    ######### use kl-div to fix min/max
     for i in range(len(weight_float_tensor[0])):
         hist, hist_edges = np.histogram(weight_float_tensor[:, i], bins=1000)
-        print(len(hist))
-        print(len(hist_edges))
+        #print(len(hist))
+        #print(len(hist_edges))
         hist_edges_min = np.amin(hist_edges)
         hist_edges_max = np.amax(hist_edges)
         hist_len=len(hist)
         zero_bin=int((-hist_edges_min*hist_len)/(hist_edges_max-hist_edges_min))
-        print('zero_bin :', zero_bin)
-        print('Sum of hist :',sum(hist))
+        #print('zero_bin :', zero_bin)
+        #print('Sum of hist :',sum(hist))
 
         H_pos=hist[zero_bin:]
         H_neg=hist[0:zero_bin]
         NUM_BINS=128
         #NUM_BINS=127
-        pos_idx, pos_threshold=get_pos_threshold(H_pos,NUM_BINS,hist_edges_max, hist_edges[zero_bin:])
-        neg_idx, neg_threshold=get_neg_threshold(H_neg,NUM_BINS,hist_edges_min, hist_edges[0:zero_bin+1])
+        #pos_idx, pos_threshold=get_pos_threshold(H_pos,NUM_BINS,hist_edges_max, hist_edges[zero_bin:])
+        #neg_idx, neg_threshold=get_neg_threshold(H_neg,NUM_BINS,hist_edges_min, hist_edges[0:zero_bin+1])
+
+        #call symmetric
+        sym_max_thresh, sym_min_thresh, sym_idx = get_symmetric_threshold(hist, 255, hist_edges_max, hist_edges_min, hist_edges[zero_bin:], hist_edges)
+
+        print('symmetric thgresholds : max/min/idx :', sym_max_thresh, sym_min_thresh, sym_idx)
+
+        #thresholds.append(sym_max_thresh)
+        #thresholds.append(sym_min_thresh)
+        pos_threshold = sym_max_thresh
+        neg_threshold = sym_min_thresh
 
         max_list.append(pos_threshold)
         min_list.append(neg_threshold)
-        print('loop :', i)
+        #print('loop :', i)
+        print('column, actual min/max and kl fixed min/max :', i, np.min(weight_float_tensor[:,i]), np.max(weight_float_tensor[:,i]), neg_threshold, pos_threshold)
 
     np.save('min_list', min_list)
     np.save('max_list', max_list)
-    
+     
     min_list = np.load('min_list.npy')
     max_list = np.load('max_list.npy')
+    
     
 
     abs_max = np.abs(max_list)
@@ -91,19 +140,21 @@ def generate_const(gr, all_const, _node_infor, a_min, a_max ):
     print('ranges :', ranges)
     '''    
 
+    '''
     ranges = np.abs(weight_float_tensor).max(axis=(0))
     min_list = np.min(weight_float_tensor, axis=0)
     max_list = np.max(weight_float_tensor, axis=0)
     print('min_list :', min_list)
     print('max_list :', max_list)
     print('ranges :', ranges)
-    
+    '''
 
     # nudging min-max values outside epsilon radius around zero
     #ranges[ranges < epsilon] = epsilon
     #min_list[np.abs(min_value) < epsilon] = -epsilon
     #max_list[np.abs(max_value) < epsilon] = epsilon
     weight_qint8_tensor = (weight_float_tensor * 127.0 / ranges).astype(np.int8)
+    #print('sizeof weight_qint8_tensor', weight_qint8_tensor.shape)
     print('weight_float_tensor :')
     print(weight_float_tensor)
     #print(weight_qint8_tensor)
@@ -129,11 +180,12 @@ def generate_const(gr, all_const, _node_infor, a_min, a_max ):
     bias_scale = 255.0 * 127.0 / ((a_max - a_min) * ranges)
     ws8_1 = np.sum(np.array(weight_qint8_tensor, dtype=np.int32),axis=0,dtype=np.int32)
     qint32_bias = np.around((bias_float_tensor * bias_scale) + (ws8_1*QaAmin))
+    #qint32_bias = np.around((ws8_1*QaAmin))
 
 
     #print(bias_scale)
     #print(ws8_1)
-    #print(qint32_bias)
+    print(qint32_bias)
     '''
     for bias_index, value in enumerate(
             np.sum(np.array(weight_qint8_tensor, dtype=np.int32),
@@ -156,13 +208,11 @@ u           int32_bias.append(
     all_const["w"] = weight_qint8_tensor
     all_const["w_min"] = min_list #min_value
     all_const["w_max"] = max_list #max_value
+    #all_const["bias_float"] = bias_float_tensor #max_value
     
     _node_infor["const_w"]["const_v"] = weight_qint8_tensor
     _node_infor["const_w_min"]["const_v"] = min_list #min_value
     _node_infor["const_w_max"]["const_v"] = max_list #max_value
     _node_infor["const_bz_qint32"]["const_v"] = qint32_bias
-    #_node_infor["const_bz_qint32"]["shape_c"] = input_shape
     #_node_infor["const_bias"]["const_v"] = bias_float_tensor
-    #_node_infor["const_wh"]["const_v"] = w_h
-    #_node_infor["const_wl"]["const_v"] = w_l
 